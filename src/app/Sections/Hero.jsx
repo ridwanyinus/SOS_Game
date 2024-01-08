148393d73c024260db3b682502183ef5acb83ef3
officialyaro9 <ridwanyinusa15@gmail.com>
2024-01-08 14:53:19 +0100

    hero-height

--- src/app/Sections/Hero.jsx
+++ src/app/Sections/Hero.jsx
@@ -4,7 +4,7 @@ import { FaDownload } from "react-icons/fa";
 import { MdOutlineGetApp } from "react-icons/md";
 const Hero = () => {
   return (
-    <main className="z-10 relative flex flex-col justify-center items-center place-content-center mt-24 xl:mt-48 ">
+    <main className="z-10 relative flex flex-col justify-center items-center place-content-center mt-28 xl:mt-48 ">
       <h1 className="font-bebas text-white font-normal text-4xl xs:text-[52px] sm:text-[70px] md:text-[80px] tracking-[3px] leading-[0x] ">SURVIVE AT ALL COSTS</h1>
       <p className="text-white font-normal tracking-[2px] text-lg xs:text-xl  md:text-2xl font-bebas xs:my-2 sm:my-6 text-center ">Experience new social battle royale game</p>
 
